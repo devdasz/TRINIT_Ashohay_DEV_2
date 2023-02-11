69acd0db6799e223f18c9bf8159a32d704f29a2a
Chayan Sarkar <59403799+apu-hub@users.noreply.github.com>
2023-02-11 14:58:05 +0530

    add tab close & request storage

--- extension/background.js
+++ extension/background.js
@@ -8,6 +8,42 @@ chrome.webRequest.onCompleted.addListener
 
 function TrackRequest(info) {
     console.log(info);
+
+    // get content lenght
+    let contentLength = 0
+    info.responseHeaders.map((e) => {
+        if (e.name === 'content-length')
+            contentLength = parseInt(e.value)
+    })
+
+    // get initiator 
+    let url = new URL(info.url)
+    if (info.type != 'main_frame') {
+        url = new URL(info.initiator)
+    }
+    // prepare domain
+    let domain = url.protocol + "//" + url.hostname
+    // console.log(domain);   // Only Dev
+    let payload = {
+        initiator: domain,
+        content_length: contentLength,
+    }
+    // console.log(payload);   // Only Dev
+
+    // get object by domain
+    let object_by_domain = getLocal(payload.initiator) == "" ? {} : JSON.parse(getLocal(payload.initiator))
+
+    object_by_domain.initiator = payload.initiator
+    object_by_domain.request_count = (object_by_domain.request_count ?? 0) + 1
+    object_by_domain.content_length = (object_by_domain.content_length ?? 0) + payload.content_length
+
+    console.log(object_by_domain);   // Only Dev
+
+    // set domain object 
+    setLocal(payload.initiator, JSON.stringify(object_by_domain))
+
+
+    // setLocal("iiii", "me")
 }
 
 
@@ -22,3 +58,27 @@ chrome.contextMenus.create({
     "contexts": ["page"],
     "onclick": OnClickShare,
 })
+
+
+chrome.tabs.onRemoved.addListener(function (tabid, removed) {
+    console.log("tab close", tabid, removed);   // Only Dev
+})
+
+
+function setLocal(key, value) {
+    // let payload = {}
+    // payload[key] = value
+    // // chrome.storage.local.set(payload)
+    // chrome.storage.sync.set({ key: value }).then(() => {
+    //     console.log("Value is set to " + value);
+    // });
+    localStorage.setItem(key, value)
+
+}
+
+function getLocal(key) {
+    let value = ""
+    value = localStorage.getItem(key) ?? ""
+    // value = await chrome.storage.local.get([key]) ?? ""
+    return value
+}
